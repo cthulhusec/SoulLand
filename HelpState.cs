e5a94b7701947d18d6ad432031737cae8fd0c9a9
landmachine <landluke11@gmail.com>
2017-02-21 09:36:10 -0600

    Update HelpState.cs

--- HelpState.cs
+++ HelpState.cs
@@ -38,7 +38,21 @@ namespace SoulLand
 			switch (switchCase)
 			{
 			case 0:
-				spriteBatch.DrawString(mainTextFont, "PUT TEXT HERE", new Vector2((game.baseScreenSize.X / 2) - (mainTextFont.MeasureString("PUT TEXT HERE").X / 2), game.baseScreenSize.Y / 2), Color.AntiqueWhite);
+				spriteBatch.DrawString(mainTextFont, "Move Using W A S D", new Vector2((game.baseScreenSize.X / 2) - (mainTextFont.MeasureString("Move Using W A S D").X / 2), game.baseScreenSize.Y / 2), Color.AntiqueWhite);
+			case 1:
+				spriteBatch.DrawString(mainTextFont, "Pick Up Items With F", new Vector2((game.baseScreenSize.X / 2) - (mainTextFont.MeasureString("Pick Up Items With F").X / 2), game.baseScreenSize.Y / 2), Color.AntiqueWhite);
+			case 2:
+				spriteBatch.DrawString(mainTextFont, "Use Q to Zoom In and E to Zoom Out", new Vector2((game.baseScreenSize.X / 2) - (mainTextFont.MeasureString("Use Q To Zoom In and E to Zoom Out").X / 2), game.baseScreenSize.Y / 2), Color.AntiqueWhite);
+			case 3:
+				spriteBatch.DrawString(mainTextFont, "Open Inventory With I", new Vector2((game.baseScreenSize.X / 2) - (mainTextFont.MeasureString("Open Inventory With I").X / 2), game.baseScreenSize.Y / 2), Color.AntiqueWhite);
+			case 4:
+				spriteBatch.DrawString(mainTextFont, "Attack Mobs with the Arrow Keys", new Vector2((game.baseScreenSize.X / 2) - (mainTextFont.MeasureString("Attack Mobs with the Arrow Keys").X / 2), game.baseScreenSize.Y / 2), Color.AntiqueWhite);
+			case 5:
+				spriteBatch.DrawString(mainTextFont, "Move Onto Doors to Open Them", new Vector2((game.baseScreenSize.X / 2) - (mainTextFont.MeasureString("Move Onto Doors to Open Them").X / 2), game.baseScreenSize.Y / 2), Color.AntiqueWhite);
+			case 6:
+				spriteBatch.DrawString(mainTextFont, "Kill the Boss on Each Level to Gain the Key that Opens the Final Door", new Vector2((game.baseScreenSize.X / 2) - (mainTextFont.MeasureString("Kill the Boss on Each Level to Gain the Key that Opens the Final Door").X / 2), game.baseScreenSize.Y / 2), Color.AntiqueWhite);
+			case 7:
+				spriteBatch.DrawString(mainTextFont, "Attempt to Survive All Levels to Win!", new Vector2((game.baseScreenSize.X / 2) - (mainTextFont.MeasureString("Attempt to Survive All Levels to Win!").X / 2), game.baseScreenSize.Y / 2), Color.AntiqueWhite);
 				break;
 			default:
 				game.ChangeState(MainGame.GameState.MainMenu);
